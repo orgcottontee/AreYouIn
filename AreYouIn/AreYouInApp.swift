0f57742015132370172116efe8b36daf29a1eb2a
cath <tsailingyu@pm.me>
2024-03-14 09:55:48 -0400

    Created project and README

---
+++ AreYouIn/AreYouInApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreYouInApp.swift
+//  AreYouIn
+//
+//  Created by adobada on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AreYouInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
